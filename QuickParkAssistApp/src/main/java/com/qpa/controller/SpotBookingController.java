1491a606035e7aa7260949794459f728604822af
samarthkoli <154528119+SamarthKoli@users.noreply.github.com>
2025-03-26 15:53:51 +0530

    Spot Booking  module backend integration complete

--- QuickParkAssistApp/src/main/java/com/qpa/controller/SpotBookingController.java
+++ QuickParkAssistApp/src/main/java/com/qpa/controller/SpotBookingController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qpa.entity.SpotBookingInfo;
@@ -23,6 +25,7 @@ import com.qpa.service.SpotBookingService;
 
 @RestController
 @RequestMapping("/bookSlot")
+@CrossOrigin(origins = "http://localhost:7213") // Allow specific origin
 public class SpotBookingController {
 
     @Autowired
