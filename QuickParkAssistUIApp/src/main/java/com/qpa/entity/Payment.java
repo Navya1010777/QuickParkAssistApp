178889c1e61e2ad0df28d300e2bc13c78ae9361a
Divakar Sharma <ds9923110@gmail.com>
2025-03-28 16:26:07 +0530

    ADDED PAYMENT M

--- QuickParkAssistUIApp/src/main/java/com/qpa/entity/Payment.java
+++ QuickParkAssistUIApp/src/main/java/com/qpa/entity/Payment.java
@@ -1,5 +1,84 @@
 package com.qpa.entity;
 
+import java.time.LocalDateTime;
+
 public class Payment {
+
+    private Long id;
+    private String  bookingId;
+    private String userEmail;
+    private Double TotalAmount;
+    private String orderId;
+    private String paymentStatus;
+    private LocalDateTime paymentTime;
+
+    public Payment() {}
+
+    public Payment(String  bookingId, String userEmail, Double TotalAmount, String orderId, String paymentStatus) {
+        this.bookingId = bookingId;
+        this.userEmail = userEmail;
+        this.TotalAmount = TotalAmount;
+        this.orderId = orderId;
+        this.paymentStatus = paymentStatus;
+        this.paymentTime = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Double getTotalAmount() {
+        return TotalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        TotalAmount = totalAmount;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public LocalDateTime getPaymentTime() {
+        return paymentTime;
+    }
+
+    public void setPaymentTime(LocalDateTime paymentTime) {
+        this.paymentTime = paymentTime;
+    }
+
+    
 
 }
