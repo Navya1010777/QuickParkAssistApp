173c4a10d1922072ffc07e66e6ca9d5983d98975
Divakar Sharma <ds9923110@gmail.com>
2025-04-02 20:07:27 +0530

    ADDon related updates

--- quick-park-assist-ui-app/src/main/java/com/qpa/controller/AddOnUIController.java
+++ quick-park-assist-ui-app/src/main/java/com/qpa/controller/AddOnUIController.java
@@ -1,6 +1,23 @@
 package com.qpa.controller;
 
-public class AddOnUIController
-{
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+
+
+@Controller
+public class AddOnUIController {
+
+    @GetMapping("/view-addon")
+    public String viewAddOnPage() {
+        return "admin/viewADDon";
+    }
+    @GetMapping("/new-addon")
+    public String getMethodName() {
+        return "admin/newADDon";
+    }
+    @GetMapping("/status")
+    public String getMethodStatus() {
+        return "admin/statusADDon";
+    }
 }
